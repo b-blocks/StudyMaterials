f1e5d28c45153eac9dbb4510ea357f447c2828dd
byungwoo <blitzblock.2025@gmail.com>
2025-10-18 20:41:24 +0900

    add

--- post-process-markdown.sh
+++ post-process-markdown.sh
@@ -3,10 +3,11 @@
 # 현재 디렉토리 및 하위 디렉토리에서 .md 파일을 찾아 처리합니다.
 find . -type f -name "*.md" | while read -r file; do
   echo "Processing $file..."
-  # macOS와 BSD sed를 위한 -i '' 옵션. GNU sed에서는 -i만 사용합니다.
-  # ### Description, 빈 줄, N/A 패턴을 찾아서 삭제.
-  # -E 옵션은 확장 정규 표현식을 사용하기 위함입니다.
-  sed -i '' -E '/^### Description$/{N;N;s/### Description\n\nN\/A//g;}' "$file"
+  # macOS/BSD sed를 위한 스크립트
+  # 1. ### Description, 빈 줄, N/A 패턴을 찾아 새 줄(\n) 하나로 치환합니다.
+  # 2. 3줄 이상의 연속된 빈 줄을 하나의 빈 줄로 압축합니다.
+  sed -i '' -e '/^### Description$/{N;N;s/### Description\n\nN\/A/\n/;}' \
+             -e '/^$/{N;/^\n$/D;}' "$file"
 done
 
 echo "Done."
