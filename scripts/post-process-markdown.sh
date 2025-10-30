73347b0204ea9f52075d7f9ff93ebd710d223cc4
byungwoo <blitzblock.2025@gmail.com>
2025-10-30 22:05:54 +0900

    Modify markdown tag

--- scripts/post-process-markdown.sh
+++ scripts/post-process-markdown.sh
@@ -5,15 +5,13 @@
 ##########################
 find ../ -type f -name "*.md" | while read -r file; do
   echo "Processing $file..."
-  # macOS/BSD sed를 위한 스크립트. 다음 네 가지 작업을 순차적으로 수행합니다.
-  # 1. "### ### "를 "### "로 변경합니다.
-  # 2. "YouTube 영상 학습 자료: " 라는 줄을 삭제합니다.
-  # 3. Description, 빈 줄, N/A 패턴을 찾아 새 줄(\n) 하나로 치환합니다.
-  # 4. 3줄 이상의 연속된 빈 줄을 하나의 빈 줄로 압축합니다.
-  sed -i '' -e 's/### ### /### /g' \
-             -e '/YouTube 영상 학습 자료: /d' \
-             -e '/^### Description$/{N;N;s/### Description\n\nN\/A/\n/;}' \
-             -e '/^$/{N;/^\n$/D;}' "$file"
+  # 1. "### ### "로 시작하는 줄을 "### "로 변경
+  sed -i '' 's/^### ### /### /g' "$file"
+  sed -i '' 's/^#### ### /#### /g' "$file"
+  sed -i '' 's/^#### #### /#### /g' "$file"
+  sed -i '' 's/^##### ### /##### /g' "$file"
+  
+  # 2. 3줄 이상의 연속된 빈 줄을 하나의 빈 줄로 압축
+  sed -i '' '/^$/N;/^\n$/N;/^\n\n$/d' "$file"
 done
-
 echo "Done."
