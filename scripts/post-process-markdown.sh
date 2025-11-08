361700f0f749e2a827b0e2f84e3e0b2a7700c657
byungwoo <byungwoo@yewonui-MacBookPro.local>
2025-11-08 13:37:09 +0900

    Add

--- scripts/post-process-markdown.sh
+++ scripts/post-process-markdown.sh
@@ -2,6 +2,7 @@
 
 ##########################
 # Markdown 후처리 스크립트 
+# Markdown 후처리 스크립트
 ##########################
 # find ../ -type f -name "*.md" | while read -r file; do
 #   echo "Processing $file..."
@@ -34,6 +35,7 @@ find ../ -type f -name "*.md" | while read -r file; do
   sed -i '' 's/# 장 제목:/#/g' "$file"
   sed -i '' 's/# 제목:/#/g' "$file"
   sed -i '' 's/URL://g' "$file"
+    sed -i '' 's/```text/```/g' "$file"
   sed -i '' 's/용어 해설 (Glossary)/용어 해설/g' "$file"
   sed -i '' 's/CTA (Call to Action)/CTA/g' "$file"
   sed -i '' 's/핵심 요약 (Executive Summary)/핵심 요약/g' "$file"
@@ -48,5 +50,27 @@ find ../ -type f -name "*.md" | while read -r file; do
   sed -i '' 's/다음은 YouTube 영상의 자막 내용을.*자료로 재구성한 문서입니다.//g' "$file"
   # 2. 3줄 이상의 연속된 빈 줄을 하나의 빈 줄로 압축
   sed -i '' '/^$/N;/^\n$/N;/^\n\n$/d' "$file"
+  # 여러 sed 명령을 하나로 통합하여 효율성 증대
+  sed -i '' -e 's/^### ## /### /g' \
+    -e 's/^### ### /### /g' \
+    -e 's/^#### ### /#### /g' \
+    -e 's/^#### #### /#### /g' \
+    -e 's/^##### ### /##### /g' \
+    -e 's/# 장 제목:/#/g' \
+    -e 's/# 제목:/#/g' \
+    -e 's/URL://g' \
+    -e 's/용어 해설 (Glossary)/용어 해설/g' \
+    -e 's/CTA (Call to Action)/CTA/g' \
+    -e 's/핵심 요약 (Executive Summary)/핵심 요약/g' \
+    -e 's/핵심 요약 (Hook)/핵심 요약/g' \
+    -e 's/상세 내용 (Detailed Content)/상세 내용/g' \
+    -e 's/상세 내용 (Storyline)/상세 내용/g' \
+    -e 's/참고 자료 (References & Further Reading)/참고 자료/g' \
+    -e 's/개요 (Overview)/개요/g' \
+    -e 's/YouTube 영상.*자료: //g' \
+    -e 's/다음은 YouTube 영상의 자막 내용을.*학습 자료입니다.//g' \
+    -e 's/다음은 YouTube 영상의 자막 내용을.*자료로 재구성한 문서입니다.//g' \
+    -e 's/```text/```/g' \
+    -e '/^$/N;/^\n$/N;/^\n\n$/d' "$file"
 done
 echo "Done."
